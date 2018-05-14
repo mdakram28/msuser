130afb8a6043f0f53fa73d2f1e78cb9c4c8a2837
Mohd Akram Ansari <MAnsari15@slb.com>
2018-05-14 09:02:22 +0530

    final

--- src/main/java/com/example/msuser/dao/UserDAO.java
+++ src/main/java/com/example/msuser/dao/UserDAO.java
@@ -6,7 +6,10 @@ import com.example.msuser.beans.User;
 
 public interface UserDAO {
 	public void insert(User user);
+
 	public User findByUsername(String username);
+
 	boolean usernameAvailable(String username);
+
 	Stream<User> getAllUsersStream();
 }
